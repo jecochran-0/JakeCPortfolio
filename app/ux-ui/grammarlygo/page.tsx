80387309609c9fc6768bb21e77d7d8bcaa94c336
jecochran-0 <jake.e.cochran@gmail.com>
2025-10-01 09:25:08 -0400

    Fix syntax error: remove unused FaArrowRight import

--- app/ux-ui/grammarlygo/page.tsx
+++ app/ux-ui/grammarlygo/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { motion } from "framer-motion";
-import { FaArrowRight } from "react-icons/fa";
 // Link removed - using <a> tags for page transition system
 
 export default function GrammarlyGOCaseStudy() {
